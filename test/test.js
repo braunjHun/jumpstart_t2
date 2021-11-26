f7c6497d31eb034369ff4aecff961704bd9d27e5
braunjHun <braunjozsefsoftware@gmail.com>
2021-11-26 15:27:13 +0100

    test: US2S2 🔴

--- test/test.js
+++ test/test.js
@@ -13,5 +13,10 @@ describe('Mine Sweeper', () => {
       application.takeStep([[1][1]]);
       expect(application.drawBoard()).toEqual("+-+-+-+\n| | | |\n+-+-+-+\n| |X| |\n+-+-+-+\n| | | |\n+-+-+-+\n\n[Sandbox 3x3] BOOM! – Game Over.");
     });
+    it('GIVEN step to position 0;2 WHEN drawing the board THEN I will see an X in 0;2 on the board and the message BOOM! – Game Over', () => {
+      const application = new Application();
+      application.takeStep([[0][2]]);
+      expect(application.drawBoard()).toEqual("+-+-+-+\n| | |X|\n+-+-+-+\n| | | |\n+-+-+-+\n| | | |\n+-+-+-+\n\n[Sandbox 3x3] BOOM! – Game Over.");
+    });
   });
 });
